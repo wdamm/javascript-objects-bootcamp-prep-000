5d886a7b8a30b81e3686fe3c75e9f1359a5ce240
William Damm <wdamm@powerhrg.com>
2018-12-30 20:44:55 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {
+  manchesterOrchestra: 'the gold',
+  houses: 'fast talk',
+  jackWhite: 'seven nation army'
+}
+
+function updatePlaylist(list,artist,song) {
+  list.artist = song;
+}
